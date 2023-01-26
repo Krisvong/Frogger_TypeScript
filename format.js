a245451a9e548927add6357eb8603f6159f8fbcc
Kristen von Gerichten <kristenvongerichten@Kristens-MBP.lan>
2023-01-25 19:52:30 -0500

    created five canvas elements and created two new js files, utilites and obstacles

---
+++ format.js
@@ -0,0 +1,29 @@
+//create five canvas elements, get each one by its id(canvas1-canvas5).
+//get the 2D rendering context of the canvas element and set the width and height of each canvas element to 600 X 600 pixels.
+const canvas = document.getElementById('canvas1');
+const ctx1 = canvas.getContext('2d');
+canvas.width = 600;
+canvas.height = 600;
+
+const canvas2 = document.getElementsById('canvas2');
+const ctx2 = canvas.getContext('2d');
+canvas.width = 600;
+canvas.height = 600;
+
+const canvas3 = docment.getElementsById('canvas3');
+const ctx3 = canvas.getContext('2d');
+canvas.width = 600;
+canvas.height = 600;
+
+const canvas4 = document.getElementsById('canvas4');
+const ctx4 = canvas.getContext('2d');
+canvas.width = 600;
+canvas.height = 600;
+
+const canvas5 = document.getElementsById('canvas5');
+const ctx5 = canvas.getContext('2d');
+canvas.width = 600;
+canvas.height = 600;
+
+//global variables
+
