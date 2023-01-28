788fd785bf370d96d220448a7cea408038a3e02e
Kristen von Gerichten <kristenvongerichten@Kristens-MBP.lan>
2023-01-28 11:39:25 -0500

    updated handleParticles function to remove particles if there are too many. Changed opacity so particles slowly fade and radius size so particles shrink away

--- particles.js
+++ particles.js
@@ -10,28 +10,42 @@ class Particle {
     }
     draw(){
         //draw circle particles under frogger
-        ctx3.fillStyle = 'rgba(150, 150, 150, 1)';
+        ctx3.fillStyle = 'rgba(150, 150, 150,' + this.opacity + ')';
         ctx3.beginPath();
         ctx3.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
         ctx3.fill();
         ctx3.closePath();
     }
     update(){
-        //run every frame to move particles along x and y axis based on values in directionX and directionY values
+        //run every frame to move particles along x and y axis based on values in directionX and directionY values.
         this.x += this.directionX;
         this.y += this.directionY;
-
+        // decrease opacity by 0.9 each time the method is called, as long as the opacity is greater than 0.1.
+        if (this.opacity > 0.1){
+            this.opacity -= 0.9;
+        }
+        //shrink the particle over time by decreasing radius by 0.14 if radius is greater than 0.15
+        if (this.radius > 0.15){
+            this.radius -= 0.14;
+        }
     }
 }
 
 function handleParticles(){
-//iterate through the particles array, call update and draw methods on each particle. 
+    //iterate through the particles array, call update and draw methods on each particle. 
     for (let i = 0; i < particlesArray.length; i++){
             particlesArray[i].update();
             particlesArray[i].draw();
+    }        
+    //if there are more than 300 particles, remove 30 particles using pop method
+    if (particlesArray.length > maxParticles){
+        for (let i = 0; i < 30; i++){
+        particlesArray.pop
+            
+        }
     }
- // Check if any of the arrow keys are being pressed and if frogger is on the road. If both true, add ten new particles to the beginning of the particle array using the unshfit method.
-    if (((keys[37] || keys [38] || keys[39] || keys[40])) && frogger.y > 100 && particlesArray.length < 200){
+    // Check if any of the arrow keys are being pressed and if frogger is on the road. If both true, add ten new particles to the beginning of the particle array using the unshfit method.
+    if (((keys[37] || keys [38] || keys[39] || keys[40])) && frogger.y > 100 && particlesArray.length < maxParticles + 10){
         for (let i = 0; i < 10; i++){
             particlesArray.unshift(new Particle(frogger.x, frogger.y));
         }
